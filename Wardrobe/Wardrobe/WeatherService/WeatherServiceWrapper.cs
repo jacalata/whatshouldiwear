be9e2e9d270e26e06ce7ad959adcea2fbd2f7fe6
mrranstrom <mrranstrom@gmail.com>
2014-03-24 13:27:23 -0700

    Integrated Forecast.io weather service
    Added WeatherServiceWrapper class to get daily weather from Forecast.io

---
+++ Wardrobe/Wardrobe/WeatherService/WeatherServiceWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ForecastIO;
+using Wardrobe.Models;
+
+namespace Wardrobe.WeatherService
+{
+    public static class WeatherServiceWrapper
+    {
+        //DO NOT TOUCH
+        private const string API_KEY = "2b9e4f5b9f8bd0850ea6e31eb388ad8b";
+
+        /// <summary>
+        /// Gets daily weather conditions given latitude and longitude. Currently synchronous
+        /// </summary>
+        /// <param name="latitude">lat</param>
+        /// <param name="longitude">long</param>
+        /// <returns>Weather object with condition and temperature from weather service</returns>
+        public Weather GetWeather(float latitude, float longitude)
+        {
+            var request = new ForecastIORequest(API_KEY, latitude, longitude, DateTime.Now, Unit.us);
+            var response = request.Get();
+
+            DailyForecast forecast = response.daily.data.First();
+
+
+            Temperature temperature = WeatherServiceHelper.GetDailyTemperature(forecast.apparentTemperatureMin, forecast.apparentTemperatureMax);
+            WeatherCondition condition = WeatherServiceHelper.GetDailyCondition(response.daily.icon);
+
+            return new Weather(condition, temperature);
+        }
+    }
+}
