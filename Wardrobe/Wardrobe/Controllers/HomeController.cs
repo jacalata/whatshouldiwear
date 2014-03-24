1b69b49106f3ecc9c692455315ba640e4edd71d8
Jac <jacalata@gmail.com>
2014-03-24 14:04:33 -0700

    update views

--- Wardrobe/Wardrobe/Controllers/HomeController.cs
+++ Wardrobe/Wardrobe/Controllers/HomeController.cs
@@ -12,5 +12,16 @@ namespace Wardrobe.Controllers
         {
             return View();
         }
+
+        public ActionResult GetOutfit()
+        {
+            WardRobeController wardrobe = new WardRobeController();
+            var item = wardrobe.Get(1);
+            if (item.Picture == null)
+            {
+                item.Picture = new Uri("http://en.wikipedia.org/wiki/File:Texas_Tech_Red_Raiders_baseball_cap.jpg");
+            }
+            return View("ItemView", item);
+        }
     }
 }
